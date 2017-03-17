042b671ae002f84b14b6222f1dcab647b3ce9f20
kgarsjo <krgarsjo@gmail.com>
2017-03-17 16:55:13 -0700

    Add an index page component

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function getBrowserAppExtension(isProduction) {
+    return (isProduction) ? '.min.js' : '.js';
+}
+
+export default function Index(props) {
+    return (
+        <html>
+            <body>
+                <div id="main"></div>
+                <script src={"/js/app" + getBrowserAppExtension(props.production)}></script>
+            </body>
+        </html>
+    );
+}
