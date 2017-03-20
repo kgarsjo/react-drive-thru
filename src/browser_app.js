3e2ca60080e7313cea610e0195263b92a898303a
kgarsjo <krgarsjo@gmail.com>
2017-03-19 17:46:32 -0700

    Update browser app to render app component

--- src/browser_app.js
+++ src/browser_app.js
@@ -1,3 +1,8 @@
+import App from './components/app';
+import React from 'react';
+import {render} from 'react-dom';
 import store from './store';
 
 console.log(store.getState());
+
+render(<App/>, document.getElementById('main'));
